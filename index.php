5cb57f886c85fee575b947d7777afad78cc81c97
Kevin Yu <kevin.sang.yu@gmail.com>
2026-01-23 13:37:45 +1100

    Realised that I accidentally put the language folder in the db folder, which was causing it to not work... Changed the way group names are fetched, so all the groupnames are fetched first and then the database queried...

--- index.php
+++ index.php
@@ -12,39 +12,40 @@ global $DB, $USER;
 
 // 1. Get user's groups
 $courses = enrol_get_users_courses($USER->id);
+$groupnames = [];
 foreach ($courses as $course) {
     $groups = groups_get_all_groups($course->id, $USER->id);
-    $groupnames = array_map(fn($g) => $g->name, $groups);
+    $groupnames = array_merge($groupnames, array_map(fn($g) => $g->name, $groups));
     echo '<h2>Course ' . $course->shortname . ' Groups: ' . implode(', ', $groupnames) . '</h2>';
-
-    // 2. Query timetable
-    list($sqlin, $params) = $DB->get_in_or_equal($groupnames, SQL_PARAMS_NAMED);
-
-    $sql = "
-        SELECT *
-        FROM {local_koitimetable}
-        WHERE groupname $sqlin
-        ORDER BY startdate, timestart
-    ";
-
-    $records = $DB->get_records_sql($sql, $params);
-
-    // 3. Render
-    $table = new html_table();
-    $table->head = ['Group', 'Date', 'Time', 'Location'];
-
-    foreach ($records as $r) {
-        $table->data[] = [
-            s($r->groupname),
-            userdate($r->startdate, '%d %b %Y'),
-            substr($r->timestart, 0, 2) . ':' . substr($r->timestart, 2, 2)
-            . ' – ' .
-            substr($r->timeend, 0, 2) . ':' . substr($r->timeend, 2, 2),
-            s($r->building . ' ' . $r->room)
-        ];
-    }
-
-    echo html_writer::table($table);
-
-    echo $OUTPUT->footer();
 }
+
+// 2. Query timetable
+list($sqlin, $params) = $DB->get_in_or_equal($groupnames, SQL_PARAMS_NAMED);
+
+$sql = "
+    SELECT *
+    FROM {local_koitimetable}
+    WHERE groupname $sqlin
+    ORDER BY startdate, timestart
+";
+
+$records = $DB->get_records_sql($sql, $params);
+
+// 3. Render
+$table = new html_table();
+$table->head = ['Group', 'Date', 'Time', 'Location'];
+
+foreach ($records as $r) {
+    $table->data[] = [
+        s($r->groupname),
+        userdate($r->startdate, '%d %b %Y'),
+        substr($r->timestart, 0, 2) . ':' . substr($r->timestart, 2, 2)
+        . ' – ' .
+        substr($r->timeend, 0, 2) . ':' . substr($r->timeend, 2, 2),
+        s($r->building . ' ' . $r->room)
+    ];
+}
+
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();
