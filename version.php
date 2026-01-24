ec1511ea18ddadfcae53021c6e8b4eee09b54ae7
Kevin Yu <kevin.sang.yu@gmail.com>
2026-01-24 16:55:05 +1100

    The plugin is officially 'working', as it is functional. As of this moment, we could implement it, but a teacher/admin will have to upload the class schedule files manually.

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_koitimetable';
-$plugin->version   = 2026012100;
+$plugin->version   = 2026012400;
 $plugin->requires  = 2022041900; // Moodle 4.0+
